b4d2e5eb822d6bce68ccb506d993e513fc50ddfe
rajumb0232 <rajugowda0232@gmail.com>
2024-05-24 07:39:16 +0530

    deleted test cases and classes

--- E-Stores-API/src/test/java/com/devb/estores/EStoresApiApplicationTests.java
+++ E-Stores-API/src/test/java/com/devb/estores/EStoresApiApplicationTests.java
@@ -1,25 +1,8 @@
 package com.devb.estores;
 
-import com.devb.estores.controller.OptionsController;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class EStoresApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-	@Autowired
-	private OptionsController optionsController;
-
-	@Test
-	public void testGetPrimeCategories(){
-		assertEquals(HttpStatus.OK.value(), optionsController.getPrimeCategories().getStatusCode().value());
-	}
 }
