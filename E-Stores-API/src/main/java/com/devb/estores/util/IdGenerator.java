b519e5ad54f16bc58d62c29a74a0860ecc37a2ff
rajumb0232 <rajugowda0232@gmail.com>
2024-05-26 11:22:26 +0530

    Updated IdGenerator by removing unnecessary Optional use

--- E-Stores-API/src/main/java/com/devb/estores/util/IdGenerator.java
+++ E-Stores-API/src/main/java/com/devb/estores/util/IdGenerator.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public class IdGenerator implements IdentifierGenerator {
     @Override
     public Object generate(SharedSessionContractImplementor session, Object object) {
-        String id = UUID.randomUUID().toString()
+        String id = UUID.randomUUID().toString();
         String prefix = "devb";
         String year = String.valueOf(Year.now());
         return prefix + year + id;
