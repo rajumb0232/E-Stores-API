d5cab90ee16c15eb02115f384e32228ecc011d3e
rajumb0232 <rajugowda0232@gmail.com>
2024-05-28 13:27:04 +0530

    test

--- E-Stores-API/src/test/java/com/devb/estores/serviceimpl/VariantServiceImplTest.java
+++ E-Stores-API/src/test/java/com/devb/estores/serviceimpl/VariantServiceImplTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -25,6 +26,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
+@SpringBootTest
 class VariantServiceImplTest {
 
     @Mock
