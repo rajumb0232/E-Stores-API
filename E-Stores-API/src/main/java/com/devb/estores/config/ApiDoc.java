5ebcf9df9f41e71637f4b7b344775e6e49a457f7
rajumb0232 <rajugowda0232@gmail.com>
2024-07-21 18:16:06 +0530

    added swagger documentation, updated response schema in product controller

--- E-Stores-API/src/main/java/com/devb/estores/config/ApiDoc.java
+++ E-Stores-API/src/main/java/com/devb/estores/config/ApiDoc.java
@@ -1,2 +1,24 @@
-package com.devb.estores.config;public class ApiDoc {
+package com.devb.estores.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class ApiDoc {
+
+    private Info info(){
+        return new Info().title("E-Stores API")
+                .description("")
+                .version("fkv1");
+    }
+
+    @Bean
+    OpenAPI openAPI(){
+        return new OpenAPI().info(info());
+    }
+
 }
