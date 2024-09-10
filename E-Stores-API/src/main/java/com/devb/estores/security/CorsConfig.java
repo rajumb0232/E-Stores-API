69a53bc212d967e07a5942a4000814b0fcba7f33
rajumb0232 <rajugowda0232@gmail.com>
2024-09-10 18:15:40 +0530

    removed unused imports and comments from corsConfig

--- E-Stores-API/src/main/java/com/devb/estores/security/CorsConfig.java
+++ E-Stores-API/src/main/java/com/devb/estores/security/CorsConfig.java
@@ -6,9 +6,6 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
@@ -17,13 +14,10 @@ public class CorsConfig {
     @Primary
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:5173");  // Add your frontend's origin
-        configuration.addAllowedMethod("*");  // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
-        configuration.addAllowedHeader("*");  // Allow all headers
-        configuration.setAllowCredentials(true);  // If you are handling cookies or authentication
-
-        // You can also further customize CORS if needed
-        // configuration.setAllowedOrigins(List.of("https://your-production-site.com"));
+        configuration.addAllowedOrigin("http://localhost:5173");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
