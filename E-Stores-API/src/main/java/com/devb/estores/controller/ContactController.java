6e7e0eb2a02fcad9640811714f54b130a7b3364c
rajumb0232 <rajugowda0232@gmail.com>
2024-10-23 19:59:45 +0530

    refractering contact module

--- E-Stores-API/src/main/java/com/devb/estores/controller/ContactController.java
+++ E-Stores-API/src/main/java/com/devb/estores/controller/ContactController.java
@@ -2,9 +2,11 @@ package com.devb.estores.controller;
 
 import com.devb.estores.model.Contact;
 import com.devb.estores.service.ContactService;
+import com.devb.estores.util.AppResponseBuilder;
 import com.devb.estores.util.ResponseStructure;
 import com.devb.estores.requestdto.ContactRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,30 +17,36 @@ import java.util.List;
 @AllArgsConstructor
 public class ContactController {
 
-    private ContactService contactService;
+    private final ContactService contactService;
+    private final AppResponseBuilder responseBuilder;
 
     @PostMapping("/addresses/{addressId}/contacts")
     public ResponseEntity<ResponseStructure<List<Contact>>> addContact(@RequestBody ContactRequest contactRequest, @PathVariable String addressId) {
-        return contactService.addContact(contactRequest, addressId);
+        List<Contact> contacts = contactService.addContact(contactRequest, addressId);
+        return responseBuilder.success(HttpStatus.CREATED, "Contact Added", contacts);
     }
 
     @PutMapping("/contacts/{contactId}")
     public ResponseEntity<ResponseStructure<List<Contact>>> updateContact(@RequestBody ContactRequest contactRequest, @PathVariable String contactId){
-        return contactService.updateContact(contactRequest, contactId);
+        List<Contact> contacts = contactService.updateContact(contactRequest, contactId);
+        return responseBuilder.success(HttpStatus.OK, "Contact Updated", contacts);
     }
 
     @GetMapping("/contacts/{contactId}")
     public ResponseEntity<ResponseStructure<Contact>> getContactById(@PathVariable String contactId){
-        return contactService.getContactById(contactId);
+        Contact contact = contactService.getContactById(contactId);
+        return responseBuilder.success(HttpStatus.FOUND, "Contact Found", contact);
     }
 
     @GetMapping("/addresses/{addressId}/contacts")
     public ResponseEntity<ResponseStructure<List<Contact>>> getContactsByAddress(@PathVariable String addressId){
-        return contactService.getContactsByAddress(addressId);
+        List<Contact> contacts = contactService.getContactsByAddress(addressId);
+        return responseBuilder.success(HttpStatus.FOUND, "Contacts found by address", contacts);
     }
 
     @DeleteMapping("/contacts/{contactId}")
     public ResponseEntity<ResponseStructure<List<Contact>>> deleteContactById(@PathVariable String contactId){
-        return contactService.deleteContactById(contactId);
+        List<Contact> contacts = contactService.deleteContactById(contactId);
+        return  responseBuilder.success(HttpStatus.OK, "Contact deleted", contacts);
     }
 }
