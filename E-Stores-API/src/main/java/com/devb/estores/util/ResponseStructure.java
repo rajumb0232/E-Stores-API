b84dafcef5aa9f6288dc5708a3322b87a1690258
rajumb0232 <rajugowda0232@gmail.com>
2024-10-24 18:21:11 +0530

    removing unnecessary setters in ResponseStructure

--- E-Stores-API/src/main/java/com/devb/estores/util/ResponseStructure.java
+++ E-Stores-API/src/main/java/com/devb/estores/util/ResponseStructure.java
@@ -11,21 +11,6 @@ public class ResponseStructure<T> {
     private String message;
     private T data;
 
-    public ResponseStructure<T> setStatus(int status) {
-       this.status = status;
-       return this;
-    }
-
-    public ResponseStructure<T> setMessage(String message) {
-        this.message = message;
-        return this;
-    }
-
-    public ResponseStructure<T> setData(T data) {
-        this.data = data;
-        return this;
-    }
-
     public static <T> ResponseStructure<T> create(int status, String message, T data) {
         return ResponseStructure.<T>builder()
                 .status(status)
