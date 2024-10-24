c9335486b638f00e084855f9a7abb068484b585c
rajumb0232 <rajugowda0232@gmail.com>
2024-10-24 18:19:58 +0530

    removing unnecessary setters in SimpleResponseStructure

--- E-Stores-API/src/main/java/com/devb/estores/util/SimpleResponseStructure.java
+++ E-Stores-API/src/main/java/com/devb/estores/util/SimpleResponseStructure.java
@@ -10,16 +10,6 @@ public class SimpleResponseStructure {
     private int status;
     private String message;
 
-    public SimpleResponseStructure setStatus(int status) {
-        this.status = status;
-        return this;
-    }
-
-    public SimpleResponseStructure setMessage(String message) {
-        this.message = message;
-        return this;
-    }
-
     public static SimpleResponseStructure create(int status, String message) {
         return SimpleResponseStructure.builder()
                 .status(status)
