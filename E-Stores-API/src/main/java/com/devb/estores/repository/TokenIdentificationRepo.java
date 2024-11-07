57ab685eb5e123570fdf1a7f8802339847d4d1d7
rajumb0232 <rajugowda0232@gmail.com>
2024-11-07 18:27:11 +0530

    Implementing Scheduled jobs to delete all Token Id's in batch

--- E-Stores-API/src/main/java/com/devb/estores/repository/TokenIdentificationRepo.java
+++ E-Stores-API/src/main/java/com/devb/estores/repository/TokenIdentificationRepo.java
@@ -2,14 +2,23 @@ package com.devb.estores.repository;
 
 import com.devb.estores.enums.TokenType;
 import com.devb.estores.model.TokenIdentification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TokenIdentificationRepo extends JpaRepository<TokenIdentification, String> {
 
-    void deleteByExpirationBefore(LocalDateTime localDateTime);
-
     Optional<TokenIdentification> findByUsernameAndDeviceIdAndTokenType(String username, String deviceId, TokenType tokenType);
+    Page<TokenIdentification> findByExpirationBefore(LocalDateTime now, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM token_identifications WHERE expiration < :now LIMIT :batchSize", nativeQuery = true)
+    int deleteBatchByExpirationBefore(LocalDateTime now, int batchSize);
 }
