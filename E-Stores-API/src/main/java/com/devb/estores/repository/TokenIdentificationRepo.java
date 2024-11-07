3298cba96c474799513f959b0ca036bc3776fa5a
rajumb0232 <rajugowda0232@gmail.com>
2024-11-07 18:28:14 +0530

    removed unnecessary native query in tokenIdentificationRepo

--- E-Stores-API/src/main/java/com/devb/estores/repository/TokenIdentificationRepo.java
+++ E-Stores-API/src/main/java/com/devb/estores/repository/TokenIdentificationRepo.java
@@ -5,9 +5,6 @@ import com.devb.estores.model.TokenIdentification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -16,9 +13,4 @@ public interface TokenIdentificationRepo extends JpaRepository<TokenIdentificati
 
     Optional<TokenIdentification> findByUsernameAndDeviceIdAndTokenType(String username, String deviceId, TokenType tokenType);
     Page<TokenIdentification> findByExpirationBefore(LocalDateTime now, Pageable pageable);
-
-    @Modifying
-    @Transactional
-    @Query(value = "DELETE FROM token_identifications WHERE expiration < :now LIMIT :batchSize", nativeQuery = true)
-    int deleteBatchByExpirationBefore(LocalDateTime now, int batchSize);
 }
